a4c347e7f14614882dd3a0fd18761fdaddd4fc97
fuyunfeng <1069035666@qq.com>
2024-04-09 17:32:50 +0800

    规则链-flow

---
+++ src/views/things/ruleChain/flow/nodeTpl/FLOW/output.vue
@@ -0,0 +1,60 @@
+<template>
+    <Form ref="formRef" :model="formState" layout="vertical">
+
+        <Form.Item name="version" v-show="false">
+            <Input v-model:value="formState.version">
+            </Input>
+        </Form.Item>
+        <Alert>
+            <template #message>
+                规则节点名称对应于输出消息的关系类型，用于转发消息发送到调用方规则链中的其他规则节点。
+            </template>
+        </Alert>
+    </Form>
+</template>
+<script lang="ts">
+export default defineComponent({
+    name: "output",
+});
+</script>
+<script lang="ts" setup >
+import { ref, watch, defineComponent, reactive } from 'vue';
+import { Form, Input, Alert } from 'ant-design-vue';
+import { FormInstance } from 'ant-design-vue/lib/form';
+
+interface Configuration {
+    version: number
+}
+
+
+const props = defineProps({
+    configuration: {
+        type: Object as PropType<Configuration>,
+        required: true,
+    },
+
+});
+
+const formRef = ref<FormInstance>();
+
+const formState = reactive<any>({
+    version: 0,
+});
+
+watch(
+    () => props.configuration,
+    () => { formState.version = props.configuration.version },
+    { immediate: true }
+)
+
+async function getConfiguration() {
+    try {
+        return await formRef.value?.validate();
+    } catch (error: any) {
+        throw error;
+    }
+}
+
+defineExpose({ getConfiguration })
+
+</script>
