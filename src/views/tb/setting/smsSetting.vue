0abaa6cf31d6876511005087c98599528cf9d27d
fuyunfeng <1069035666@qq.com>
2024-11-18 14:53:25 +0800

    新增阿里云短信

--- src/views/tb/setting/smsSetting.vue
+++ src/views/tb/setting/smsSetting.vue
@@ -2,14 +2,49 @@
   <Spin :spinning="dataLoading">
     <div class="sms-setting">
       <div class="text-lg font-bold my-4">短信服务商配置</div>
-      <BasicForm @register="registerForm">
-
-      </BasicForm>
+      <Form ref="formRef" :model="formState" layout="vertical">
+        <Form.Item label="短信服务商" :name="['jsonValue', 'type']" :rules="[{ required: true }]">
+          <Select v-model:value="formState.jsonValue.type" :disabled="true">
+            <Select.Option value="ALI_SMS">阿里云短信</Select.Option>
+            <Select.Option value="AWS_SNS">亚马逊短信</Select.Option>
+            <Select.Option value="TWILIO">Twilio</Select.Option>
+            <Select.Option value="SMPP">SMPP</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="阿里云访问秘钥ID"
+          :name="['jsonValue', 'accessKeyId']"
+          :rules="[{ required: true }]"
+        >
+          <Input v-model:value="formState.jsonValue.accessKeyId" />
+        </Form.Item>
+        <Form.Item
+          label="阿里云访问秘钥"
+          :name="['jsonValue', 'accessKeySecret']"
+          :rules="[{ required: true }]"
+        >
+          <InputPassword v-model:value="formState.jsonValue.accessKeySecret" />
+        </Form.Item>
+        <Form.Item label="短信签名" :name="['jsonValue', 'signName']" :rules="[{ required: true }]">
+          <Input v-model:value="formState.jsonValue.signName" />
+        </Form.Item>
+        <Form.Item
+          label="短信模板 Code"
+          :name="['jsonValue', 'templateCode']"
+          :rules="[{ required: true }]"
+        >
+          <Input v-model:value="formState.jsonValue.templateCode" />
+        </Form.Item>
+      </Form>
       <div class="footer">
         <Space>
           <a-button @click="fetchData">
             <Icon :icon="'ant-design:reload-outlined'" />
             重置
+          </a-button>
+          <a-button :loading="dataLoading" @click="handleOpenTestSmsModal">
+            <Icon :icon="'ant-design:send-outlined'" />
+            发送测试短信
           </a-button>
           <a-button :loading="dataLoading" type="primary" @click="handleSubmit">
             <Icon :icon="'ant-design:check-outlined'" />
@@ -17,84 +52,166 @@
           </a-button>
         </Space>
       </div>
+      <Modal
+        v-model:open="showSmsTestModal"
+        :centered="true"
+        title="发送测试短信"
+        okText="发送测试短信"
+        @ok="handleSendTestSms"
+      >
+        <Form ref="testFromRef" :model="testSms" layout="vertical">
+          <Form.Item
+            label="手机号码"
+            name="numberTo"
+            :rules="[
+              { required: true },
+              { pattern: /^1[3-9]\d{9}$/, message: t('请填写正确的手机号码') },
+            ]"
+          >
+            <Input v-model:value="testSms.numberTo" />
+          </Form.Item>
+          <Form.Item label="短信内容" name="message" :rules="[{ required: true }]">
+            <Input.TextArea v-model:value="testSms.message" :rows="4" />
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   </Spin>
 </template>
 <script lang="ts" setup name="ViewsTbSettingSmsSetting">
-import { ref, unref, onMounted } from 'vue';
-import { Icon } from '/@/components/Icon';
-import { useI18n } from '/@/hooks/web/useI18n';
-import { useMessage } from '/@/hooks/web/useMessage';
-import { router } from '/@/router';
-import { BasicForm, FormSchema, useForm } from '/@/components/Form';
-import { getAdminSetting, saveAdminSetting, AdminSetting } from '/@/api/tb/adminSetting';
-import { Space, Spin } from 'ant-design-vue';
+  import { ref, onMounted, reactive } from 'vue';
+  import { Icon } from '/@/components/Icon';
+  import { useUserStore } from '/@/store/modules/user';
+  import { FormInstance } from 'ant-design-vue/lib/form';
+  import { useI18n } from '/@/hooks/web/useI18n';
+  import { useMessage } from '/@/hooks/web/useMessage';
+  import { getAdminSetting, saveAdminSetting, AdminSetting } from '/@/api/tb/adminSetting';
+  import { sendTestSms } from '/@/api/tb/adminSetting';
+  import { Space, Select, Spin, Form, Input, InputPassword, Modal } from 'ant-design-vue';
 
-const { t } = useI18n('tb');
-const { showMessage } = useMessage();
-const { meta } = unref(router.currentRoute);
-const record = ref<AdminSetting>({} as AdminSetting);
-const dataLoading = ref(false);
+  const { t } = useI18n('tb');
+  const userStore = useUserStore();
+  const { showMessage } = useMessage();
+  const formRef = ref<FormInstance>();
+  const testFromRef = ref<FormInstance>();
+  const record = ref<AdminSetting>({} as AdminSetting);
+  const dataLoading = ref(false);
+  const showSmsTestModal = ref(false);
 
-const inputFormSchemas: FormSchema[] = [
-];
+  const formState = reactive<AdminSetting>({
+    tenantId: userStore.getUserInfo.tenantId,
+    key: 'sms',
+    jsonValue: {
+      accessKeyId: '',
+      accessKeySecret: '',
+      signName: '',
+      templateCode: '',
+      type: 'ALI_SMS',
+    },
+  } as AdminSetting);
 
-const [registerForm, { resetFields, setFieldsValue, getFieldsValue, validate }] = useForm({
-  labelAlign: 'left',
-  labelWidth: 120,
-  schemas: inputFormSchemas,
-  baseColProps: { lg: 12, md: 24 },
+  const testSms = reactive<any>({
+    message: '',
+    numberTo: '',
+  });
 
-});
-
-onMounted(() => {
-  fetchData();
-})
-
-async function fetchData() {
-  try {
-    dataLoading.value = true;
-    const result = await getAdminSetting('sms');
-    record.value = result
-    await setFieldsValue(record.value);
-  } catch (error: any) {
-    if (error && error.errorFields) {
-      showMessage(t('common.validateError'));
-    }
-    console.log('error', error);
-  } finally {
-    dataLoading.value = false;
-  }
-};
-
-async function handleSubmit() {
-  try {
-    const data = await validate();
-    dataLoading.value = true;
-    // console.log('submit', params, data, record);
-    const res = await saveAdminSetting({ ...record.value, ...data });
+  onMounted(() => {
     fetchData();
-    showMessage('保存通用配置成功！');
-  } catch (error: any) {
-    if (error && error.errorFields) {
-      showMessage(t('common.validateError'));
+  });
+
+  async function fetchData() {
+    try {
+      dataLoading.value = true;
+      clear();
+      const result = await getAdminSetting('sms');
+      record.value = result;
+      Object.keys(record.value).forEach((key) => {
+        formState[key] = record.value[key];
+      });
+      formState.jsonValue.type = 'ALI_SMS';
+    } catch (error: any) {
+      if (error && error.errorFields) {
+        showMessage(t('common.validateError'));
+      }
+      console.log('error', error);
+    } finally {
+      dataLoading.value = false;
     }
-    console.log('error', error);
-  } finally {
-    dataLoading.value = false;
   }
-}
 
+  function clear() {
+    formState.tenantId = userStore.getUserInfo.tenantId;
+    formState.key = 'sms';
+    formState.jsonValue = {
+      accessKeyId: '',
+      accessKeySecret: '',
+      signName: '',
+      templateCode: '',
+      type: 'ALI_SMS',
+    };
+    testSms.message = '';
+    testSms.numberTo = '';
+  }
 
+  async function handleSubmit() {
+    try {
+      const data = await formRef.value?.validate();
+      dataLoading.value = true;
+      // console.log('submit', params, data, record);
+      const res = await saveAdminSetting({ ...record.value, ...data });
+      fetchData();
+      showMessage('保存短信配置配置成功！');
+    } catch (error: any) {
+      if (error && error.errorFields) {
+        showMessage(t('common.validateError'));
+      }
+      console.log('error', error);
+    } finally {
+      dataLoading.value = false;
+    }
+  }
 
+  async function handleOpenTestSmsModal() {
+    try {
+      await formRef.value?.validate();
+      showSmsTestModal.value = true;
+    } catch (error: any) {
+      if (error && error.errorFields) {
+        showMessage(t('common.validateError'), 'error');
+      }
+      console.log('error', error);
+    }
+  }
+  async function handleSendTestSms() {
+    try {
+      const provider = await formRef.value?.validate();
+      const data = await testFromRef.value?.validate();
+      dataLoading.value = true;
+      if (provider && data) {
+        const result = await sendTestSms({
+          numberTo: '+86' + data.numberTo,
+          message: data.message,
+          providerConfiguration: provider.jsonValue,
+        });
+        showMessage('发送测试短信成功！');
+      }
+    } catch (error: any) {
+      if (error && error.errorFields) {
+        showMessage(t('common.validateError'), 'error');
+      }
+      console.log('error', error);
+    } finally {
+      dataLoading.value = false;
+      showSmsTestModal.value = false;
+    }
+  }
 </script>
 <style lang="less">
-.sms-setting {
-  padding: 6px 46px 24px 24px;
+  .sms-setting {
+    padding: 6px 46px 24px 24px;
 
-  .footer {
-    margin-top: 42px;
+    .footer {
+      margin-top: 42px;
+    }
   }
-
-}
 </style>
